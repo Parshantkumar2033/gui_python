283f4e451216f1f82d01970871d4eec6943fc515
Parshantkumar2033 <parshant.kumar21b@iiitg.ac.in>
2023-03-26 22:32:50 +0530

    new file added

---
+++ icon_image.py
@@ -0,0 +1,11 @@
+from tkinter import *
+root = Tk()
+root.title("whatsapp")
+root.iconbitmap('C:/Users/PRASHANT KAJAL/Desktop/download.ico')
+
+button_exit = Button(root, text = "Exit", borderwidth= 3, command = root.quit)
+button_exit.pack()
+
+
+
+root.mainloop()
