63bc89e86184a261c6a0d42878d8842ed9e2f6c5
Parshantkumar2033 <parshant.kumar21b@iiitg.ac.in>
2023-04-01 23:27:38 +0530

    a

--- icon_image.py
+++ icon_image.py
@@ -1,11 +1,15 @@
 from tkinter import *
+from PIL import ImageTk, Image
 root = Tk()
 root.title("whatsapp")
-root.iconbitmap('C:/Users/PRASHANT KAJAL/Desktop/download.ico')
+root.iconbitmap('download.ico')
 
 button_exit = Button(root, text = "Exit", borderwidth= 3, command = root.quit)
 button_exit.pack()
 
+image = ImageTk.PhotoImage(Image.open('katakuri.png'))
+new_label = Label(image = image)
+new_label.pack()
 
 
 root.mainloop()
